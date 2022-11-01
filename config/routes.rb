1d1383aabb17d718eba082f476e9fb01d6bbfc92
EC2 Default User <110474164+Jupiter200108@users.noreply.github.com>
2022-11-01 14:46:20 +0000

    [add] mailer

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,14 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get "daily_posts" => "users#daily_posts"
   end
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+    resources :event_notices, only: [:new, :create]
+    get "event_notices" => "event_notices#sent"
+  end
+
   get "/search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :chats, only: [:show, :create]
